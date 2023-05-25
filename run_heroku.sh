809dee225b648d5250f67fd602c2343ef5a3f8bc
Jonas De Beukelaer <jonas@beacon.co.uk>
2023-05-25 10:59:34 +0300

    heroku 3

--- run_heroku.sh
+++ run_heroku.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Running trader script..."
+
 echo "$AI_SETTINGS" > ai_settings.yaml
 python -m autogpt --ai-settings ai_settings.yaml --gpt3only --skip-news --debug --continuous
+
+echo "Script finished!"
