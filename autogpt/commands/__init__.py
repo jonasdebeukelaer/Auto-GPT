7d9b54318b850ab860da9df6592c2520cd99ebe4
Jonas De Beukelaer <jonas@beacon.co.uk>
2023-07-24 22:01:49 +0100

    fix: init - don't disable commands via there
    already done in config

--- autogpt/commands/__init__.py
+++ autogpt/commands/__init__.py
@@ -1,5 +1,5 @@
 COMMAND_CATEGORIES = [
-    # "autogpt.commands.execute_code",
+    "autogpt.commands.execute_code",
     "autogpt.commands.file_operations",
     "autogpt.commands.web_search",
     "autogpt.commands.web_selenium",
