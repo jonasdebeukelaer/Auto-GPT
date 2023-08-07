bf7fc0cedb8cf51269af364a57abee5b9ef90794
Jonas De Beukelaer <jonas@beacon.co.uk>
2023-08-07 11:44:13 +0100

    refactor: namings and remove heroku stuff

--- heroku/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running trader script..."
-
-echo "$AI_SETTINGS" > ai_settings.yaml
-python -m autogpt --ai-settings ai_settings.yaml --skip-news --continuous
-
-echo "Trader script finished!"
